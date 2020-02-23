9cf8186458afe560c0f707f768b839e8c888b82f
bates-hu <hbc0204@foxmail.com>
2020-02-22 20:26:50 -0800

    use helmet to load comment module

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,23 +1,17 @@
 import React, { useEffect } from "react"
+import Helmet from "react-helmet"
 
 const Comment = () => {
-  // (title, date, desc, tags)
-  const script = document.createElement("script")
-  script.src = "https://cdn-city.livere.com/js/embed.dist.js"
-  script.async = true
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.getElementById("lv-container").appendChild(script)
-  });
-
   return (
-    <div
-      id="lv-container"
-      data-id="city"
-      data-uid="MTAyMC80ODc2MS8yNTI1NQ=="
-    ></div>
+    <div id="lv-container" data-id="city" data-uid="MTAyMC80ODc2MS8yNTI1NQ==">
+      <Helmet>
+        <script
+          src="https://cdn-city.livere.com/js/embed.dist.js"
+          async="true"
+          type="text/javascript"
+        ></script>
+      </Helmet>
+    </div>
   )
 }
 
